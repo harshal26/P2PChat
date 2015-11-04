31feb74247918412c0227397391b374172c43f8c
HARSHAL SHAH <hs26593@yahoo.com>
2015-11-04 15:44:44 -0800

    complete chat functionality

---
+++ src/com/example/p2pchat579/Constants.java
@@ -0,0 +1,5 @@
+package com.example.p2pchat579;
+
+public class Constants {
+	public static final int CHOOSE_FILE_RESULT_CODE = 3; 
+}
---
+++ src/com/example/p2pchat579/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.p2pchat579;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	
+	Button btButton, button2;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		btButton = (Button)findViewById(R.id.bluetoothButton);
+		button2 = (Button)findViewById(R.id.wifiButton);
+		
+		btButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				connectBluetooth();
+				
+			}
+		});
+		
+		button2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//connectWifi();				
+			}
+		});
+	}
+	
+	public void connectBluetooth()
+	{
+		Intent bluetoothIntent = new Intent(this,BluetoothChat.class);
+		startActivity(bluetoothIntent);
+	}
+	
+	
+	
+	
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
